79a132b149a6621b96a59906f121a48ad8c4a818
JesusGam3s <jvalenciabarroso@gmail.com>
2025-04-02 10:13:21 +0200

    upload opiniones

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,21 @@
-// Tu configuración de Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_ID_MENSAJES",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyD4UnlMAsyo90brcIx3kXTvNEk5IiDUbCM",
+  authDomain: "cafes-los-angeles-39eb0.firebaseapp.com",
+  projectId: "cafes-los-angeles-39eb0",
+  storageBucket: "cafes-los-angeles-39eb0.firebasestorage.app",
+  messagingSenderId: "504133561806",
+  appId: "1:504133561806:web:2de9c0f450b61dc9800f9f",
+  measurementId: "G-7DKM97QJMH"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- opiniones.js
+++ opiniones.js
@@ -176,4 +176,13 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
     }
+    const firebaseConfig = {
+        apiKey: "AIzaSyD4UnlMAsyo90brcIx3kXTvNEk5IiDUbCM",
+        authDomain: "cafes-los-angeles-39eb0.firebaseapp.com",
+        projectId: "cafes-los-angeles-39eb0",
+        storageBucket: "cafes-los-angeles-39eb0.firebasestorage.app",
+        messagingSenderId: "504133561806",
+        appId: "1:504133561806:web:2de9c0f450b61dc9800f9f",
+        measurementId: "G-7DKM97QJMH"
+    };
 });
