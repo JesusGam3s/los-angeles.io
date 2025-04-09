aa154e714c72f1169811a58784d636f164f35042
JesusGam3s <jvalenciabarroso@gmail.com>
2025-04-09 11:02:14 +0200

    Update sugerencias.js

--- sugerencias.js
+++ sugerencias.js
@@ -1,6 +1,6 @@
 document.getElementById("boton-sugerencia").addEventListener("click", function () {
-    const destinatario = "tucorreo@gmail.com"; // Cámbialo por el tuyo
-    const asunto = encodeURIComponent("Sugerencia para la web");
+    const destinatario = "soporte.cafes.los.angeles@gmail.com"; // Cámbialo por el tuyo
+    const asunto = encodeURIComponent("Pregunta de la pagina web");
     const cuerpo = encodeURIComponent("\n\n");
   
     const enlace = `https://mail.google.com/mail/?view=cm&fs=1&to=${destinatario}&su=${asunto}&body=${cuerpo}`;
