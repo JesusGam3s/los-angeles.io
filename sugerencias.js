cbf4ab97098133cbb4c2df03ac9a6e825f3c19a6
JesusGam3s <soporte.cafes.los.angeles@gmail.com>
2025-05-19 11:18:03 +0200

    fuera contacto

--- sugerencias.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("boton-sugerencia").addEventListener("click", function () {
-    const destinatario = "soporte.cafes.los.angeles@gmail.com"; // Cámbialo por el tuyo
-    const asunto = encodeURIComponent("Pregunta de la pagina web");
-    const cuerpo = encodeURIComponent("\n\n");
-  
-    const enlace = `https://mail.google.com/mail/?view=cm&fs=1&to=${destinatario}&su=${asunto}&body=${cuerpo}`;
-    window.open(enlace, "_blank");
-  });
-  
